75a971d054174bceefaa051357eeee9f0334f11a
Fabio Aguiar <fabio.barreto@gmail.com>
2020-07-04 11:02:36 -0300

    FAS_3.7-desafio-retornar-404-caso-nao-exista-a-categoria-v1

--- src/main/java/com/algamoney/api/controller/CategoriaController.java
+++ src/main/java/com/algamoney/api/controller/CategoriaController.java
@@ -3,11 +3,14 @@ package com.algamoney.api.controller;
 import com.algamoney.api.domain.model.Categoria;
 import com.algamoney.api.domain.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
@@ -17,7 +20,27 @@ public class CategoriaController {
     private CategoriaRepository categoriaRepository;
 
     @GetMapping
-    public List<Categoria> listar() {
-        return this.categoriaRepository.findAll();
+    public ResponseEntity<?> listar() {
+        List<Categoria> categorias = this.categoriaRepository.findAll();
+        return ResponseEntity.ok(categorias);
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+        Categoria categoriaSalva = this.categoriaRepository.save(categoria);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("{codigo}")
+                .buildAndExpand(categoria.getCodigo()).toUri();
+
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categoriaSalva);
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Categoria> buscar(@PathVariable Long codigo) {
+        Optional<Categoria> categoria = this.categoriaRepository.findById(codigo);
+
+        return !categoria.isEmpty() ? ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
     }
 }
