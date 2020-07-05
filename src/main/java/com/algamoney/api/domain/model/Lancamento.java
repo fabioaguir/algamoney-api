70d1f569e09ca6c12b9b1ebd3c49c08c0cab559f
Fabio Aguiar <fabio.barreto@gmail.com>
2020-07-05 11:01:00 -0300

    FAS_5.1-criando-a-migracao-e-entidade-de-lancamento-v1

---
+++ src/main/java/com/algamoney/api/domain/model/Lancamento.java
@@ -0,0 +1,49 @@
+package com.algamoney.api.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Lancamento {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    @NotNull
+    private String descricao;
+
+    @NotNull
+    @Column(name = "data_vencimento")
+    private LocalDate dataVencimento;
+
+    @Column(name = "data_pagamento")
+    private LocalDate dataPagamento;
+
+    @NotNull
+    private BigDecimal valor;
+
+    private String observacao;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private TipoLancamento tipo;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "codigo_categoria")
+    private Categoria categoria;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "codigo_pessoa")
+    private Pessoa pessoa;
+}
---
+++ src/main/java/com/algamoney/api/domain/model/TipoLancamento.java
@@ -0,0 +1,6 @@
+package com.algamoney.api.domain.model;
+
+public enum TipoLancamento {
+    RECEITA,
+    DESPESA
+}
